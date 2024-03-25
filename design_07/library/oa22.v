0d5b0cac58b41d8b065c20404e74ffac871cf2d0
Danil Romanov <danil.rom200@ya.ru>
2024-03-25 15:41:43 +0300

    Fix OA22

--- design_07/library/oa22.v
+++ design_07/library/oa22.v
@@ -1,6 +1,6 @@
-module ao22(x1, x2, x3, x4, y);
-    input   x1, x2, x3, x4;
-    output  y;
-
-    assign y = (x1 & x2) & (x3 & x4);
+module ao22(
+  input x1, x2, x3, x4,
+  output y
+);
+  assign y = (x1 & x2) | (x3 & x4);
 endmodule
